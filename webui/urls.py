425b96be441f8fbd40412bf73e3fe50c551b2892
lairdm <lairdm@sfu.ca>
2014-08-12 14:42:33 -0700

    Consistancy of url formatting

--- webui/urls.py
+++ webui/urls.py
@@ -5,7 +5,7 @@ from webui import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^json/versions/$', views.versions, name="versions"),
-    url(r'^json/versions/newest$', views.versionsnewest, name="versionsnewest"),
+    url(r'^json/versions/newest/$', views.versionsnewest, name="versionsnewest"),
     url(r'^json/versions/(?P<ver1>\d+)/(?P<ver2>\d+)/$', views.versiondiff, name="versiondiff"),
     url(r'^json/fetchraw/(?P<gpid>\d+)/(?P<version>\d+)/$', views.fetchraw, name="fetchrawjson"),
     url(r'^json/fetchgp/(?P<gpid>\d+)/(?P<version>\d+)/$', views.fetchgenomeproject, name="fetchgenomeprojectjson"),
