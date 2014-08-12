497a32457031cc93ca4741c9190323fa0e69779f
lairdm <lairdm@sfu.ca>
2014-08-12 12:29:06 -0700

    slight url change

--- webui/urls.py
+++ webui/urls.py
@@ -4,8 +4,8 @@ from webui import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^versions/$', views.versions, name="versions"),
-    url(r'^versions/newest$', views.versionsnewest, name="versionsnewest"),
+    url(r'^json/versions/$', views.versions, name="versions"),
+    url(r'^json/versions/newest$', views.versionsnewest, name="versionsnewest"),
     url(r'^json/fetchraw/(?P<gpid>\d+)/(?P<version>\d+)/$', views.fetchraw, name="fetchrawjson"),
     url(r'^json/fetchgp/(?P<gpid>\d+)/(?P<version>\d+)/$', views.fetchgenomeproject, name="fetchgenomeprojectjson"),
     url(r'^json/fetchgp/(?P<gpid>\d+)/$', views.fetchgenomeprojects, name="fetchgenomeprojectsjson"),
